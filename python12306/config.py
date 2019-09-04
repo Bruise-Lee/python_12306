8305ca395cf0c5e1b736816b3374a8028b79c9dd
Bruise-Lee <52093329+Bruise-Lee@users.noreply.github.com>
2019-09-05 06:44:40 +0800

    Update config.py

--- python12306/config.py
+++ python12306/config.py
@@ -1,11 +1,11 @@
 import os
 import yaml
-import namedtupled
+import namedtupled   #namedtuple，你不必再通过索引值进行访问，你可以把它看做一个字典通过名字进行访问，只不过其中的值是不能改变的。
 
 
 def parsing_config():
     """
-    解析yaml
+    解析yaml （yaml类似于xml语言，但更简洁。）
     :return: s  字典
     """
     path = os.path.join(os.getcwd(), 'config.yaml')
